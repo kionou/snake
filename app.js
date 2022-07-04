fa609b15843d3b94582a02e123255367778479a8
kionou <kionoumamadou.00@gmail.com>
2022-07-04 08:33:36 +0000

    front

--- app.js
+++ app.js
@@ -2,26 +2,15 @@
 let body = document.querySelector('.entrerNom')
 let input = document.createElement('input')
 body.prepend(input)
+let erreur= document.querySelector('.entrerNom p')
 let pseudo= document.querySelector('input') 
 let div = document.querySelector('.detail')
 let global = document.querySelector('.global')
 let contenu = document.querySelector('.tableau')
-
-
-var modal = document.getElementById("myModal");
-
- function open() {
-  modal.style.display = "block";
-}
-
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
+let modal = document.querySelector('.modal') 
 
 window.addEventListener("load",()=>{
-    
+ 
     const ws = new  WebSocket('ws:localhost:3000')
     ws.onopen =()=>{
         ws.onmessage = (i)=>{
@@ -31,14 +20,20 @@ window.addEventListener("load",()=>{
                 body.style.display='none'
                 global.style.display='flex'
                 div.innerHTML=`
-                    <p>id:${message.data.id}</p>
-                    <p>pseudo:${message.data.pseudo}</p>`
+                     <div id="contenu">
+                    <p>id:</p>
+                    <span>${message.data.id}</span>
+                </div>
+                <div id="contenu">
+                    <p>Pseudo:</p>
+                    <span>${message.data.pseudo}</span>
+                </div>
+                    `
                 message.data.users.forEach(el => {
                     contenu.innerHTML+=`
                     <div class="contenu" id=${el.id}>
                     <p>${el.pseudo}</p>
                     <button onclick="open()" id="boutton">dual</button>
-
                  </div>
                     `
                 });    
@@ -56,19 +51,22 @@ window.addEventListener("load",()=>{
         }
     }
    
-  
+    function open() {
+        // modal.style.display='block'
+        console.log('bonjour');
+      }
 
     pseudo.addEventListener('change',(e)=>{
-        console.log('hjeoo')
         let nom = e.target.value;
-        console.log(nom.length);
         if (nom.length < 5) {
-          alert("pseudo 5 caractere");
+         erreur.innerHTML=`Le pseudo doit etre au moins 5 caracteres`
         }else{
           ws.send(JSON.stringify({"type":"connection","data":nom}))
         }
       
       
       })
+
+     
 
 })
